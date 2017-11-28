b36970939083dc48814d5f7b81a8561d9a3de5dc
edelciobatista <edelcio.batista@gmail.com>
2017-11-28 21:38:59 -0200

    Padroniza nomes

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -15,3 +15,8 @@ class SubscriptionForm(forms.Form):
     email = forms.EmailField(label='Email')
     phone = forms.CharField(label='Telefone')
 
+    def clean_name(self): # 'clean_' + nome_do_campo
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
+
