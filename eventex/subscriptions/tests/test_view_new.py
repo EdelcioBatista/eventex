a64e4abfdc68005b6e7990061a8da073a4a67632
edelciobatista <edelcio.batista@gmail.com>
2017-11-29 21:55:13 -0200

    Eamil ou telefone opcionais

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -78,12 +78,12 @@ class SubscriptionsNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
-class TemplateregressionTest(TestCase):
+class TemplateRegressionTest(TestCase):
     def test_template_has_non_field_errors(self):
         invalid_data = dict(name='Edelcio Batista', cpf='12345678901')
-        #response = self.client.post(r('subscription:new'), invalid_data)
+        response = self.client.post(r('subscriptions:new'), invalid_data)
 
-        #self.assertContains(response, '<ul class="errorlist nonfield">')
+        self.assertContains(response, '<ul class="errorlist nonfield">')
 
 
 
